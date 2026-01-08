7d2d71124a6009c465a44606e09d729666ff59f2
Maxwell Walker <maxwell.a.walker@gmail.com>
2026-01-08 11:51:03 -0500

    Fix basePath to match repo name

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/personal-site',
+  basePath: '/maxwalker.me',
   images: {
     unoptimized: true,
   },
